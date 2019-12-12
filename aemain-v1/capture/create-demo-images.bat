8c9517e96028c0076064863621b1fab9a15c9756
AutomicDemo <36577894+AutomicDemo@users.noreply.github.com>
2019-12-12 00:37:03 -0800

    Update to v5

--- aemain-v1/capture/create-demo-images.bat
+++ aemain-v1/capture/create-demo-images.bat
@@ -1,5 +1,5 @@
-start capture\create_image.bat aemain-cdrive-v1-mine2 aemain-cdrive %1
-start capture\create_image.bat aemain-ddrive-v1-mine2 aemain-ddrive %1
-start capture\create_image.bat aejenkins-v1-mine aejenkins %1
-start capture\create_image.bat aecdd1-v1-mine2 aecdd1 %1
-start capture\create_image.bat aecdd2-v1-mine2 aecdd2 %1
+rem start capture\create_image.bat aemain-cdrive-v1-mine2 aemain-cdrive %1
+rem start capture\create_image.bat aemain-ddrive-v1-mine2 aemain-ddrive %1
+start capture\create_image.bat aejenkins-v1-mine2 aejenkins %1
+rem start capture\create_image.bat aecdd1-v1-mine2 aecdd1 %1
+rem start capture\create_image.bat aecdd2-v1-mine2 aecdd2 %1
